17b527cee5c7c36ea8d10f6746bf2cc9aafb309a
logosstone <p.felixgarcia@gmail.com>
2025-04-17 20:31:22 -0700

    Fix UI elements not scaling correctly on small widths (#2678)

--- webview-ui/src/components/settings/Section.tsx
+++ webview-ui/src/components/settings/Section.tsx
@@ -5,5 +5,5 @@ import { cn } from "@/lib/utils"
 type SectionProps = HTMLAttributes<HTMLDivElement>
 
 export const Section = ({ className, ...props }: SectionProps) => (
-	<div className={cn("flex flex-col gap-3 p-5", className)} {...props} />
+	<div className={cn("flex flex-col gap-3 p-3 pr-[2px] sm:pr-3", className)} {...props} />
 )
